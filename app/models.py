287e8648385e8a45e0d6b3f9218814dacd57e727
Natan619 <natateixeira11@gmail.com>
2024-11-14 16:10:32 -0300

    versão 1.1.0

--- app/models.py
+++ app/models.py
@@ -4,20 +4,20 @@ from datetime import datetime
 class Aluno(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    faturamento = db.Column(db.Float, nullable=False)  # Certifique-se de que este campo existe
+    faturamento = db.Column(db.Float, nullable=False)  
     forma_pagamento = db.Column(db.String(50), nullable=False)
-    data = db.Column(db.Date, nullable=False, default=datetime.utcnow)  # Use datetime.date se quiser apenas a data
+    data = db.Column(db.Date, nullable=False, default=datetime.utcnow)  
 
 class Artesanato(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     faturamento = db.Column(db.Float, nullable=False)
     forma_pagamento = db.Column(db.String(50), nullable=False)
-    data = db.Column(db.Date, nullable=False, default=datetime.utcnow)  # Use datetime.date se quiser apenas a data
+    data = db.Column(db.Date, nullable=False, default=datetime.utcnow)  
 
 class Gasto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome_do_gasto = db.Column(db.String(100), nullable=False)
-    valor = db.Column(db.Float, nullable=False)  # Adicionado campo de valor
+    valor = db.Column(db.Float, nullable=False)  
     forma_pagamento = db.Column(db.String(50), nullable=False)
     data = db.Column(db.Date, nullable=False, default=datetime.utcnow)
