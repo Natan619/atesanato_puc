139ae022ce5632db94f72d4e43d4154a4610a770
Natan619 <natateixeira11@gmail.com>
2024-11-21 07:49:46 -0300

    funciona plmd

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql+psycopg2://artesanato_user:123puc@localhost/artesanato_puc_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
