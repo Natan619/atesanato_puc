0a12fab49c5a8b5edeadf15bd5782c5a89bc6189
Natan619 <natateixeira11@gmail.com>
2024-11-21 06:27:04 -0300

    versão estável

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://artesanato_user:123puc@localhost/artesanato_puc_db'
+        'postgresql+psycopg2://artesanato_user:123puc@localhost/artesanato_puc_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
