4b808201ad7203cc439f7f4e1c2165cad3db3f09
Natan619 <natateixeira11@gmail.com>
2024-11-26 17:42:00 -0300

    deploy de banco

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
 class Config:
+    # Utilize a variável de ambiente DATABASE_URL
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Verifique se a variável está definida
+    if not SQLALCHEMY_DATABASE_URI:
+        raise ValueError("DATABASE_URL não está definida! Certifique-se de que está configurada corretamente no ambiente do Render.")
